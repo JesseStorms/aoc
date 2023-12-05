abd76c21584e026c12111136108f30bbcd578e35
Jesse Storms <jesse.storms@student.ucll.be>
2023-12-05 16:17:42 +0100

    day05/02 done very badly (but still committed for sake of it)
    this works if you have the pc for it

--- 2023/day05/main.go
+++ 2023/day05/main.go
@@ -13,8 +13,8 @@ import (
 var input string
 
 func main() {
-	fmt.Println("part 1:", part1(input))
-	// fmt.Println("part 2:", part2(input))
+	// fmt.Println("part 1:", part1(input))
+	fmt.Println("part 2:", part2(input))
 }
 
 func part1(input string) int {
@@ -24,15 +24,39 @@ func part1(input string) int {
 	for _, region := range regions[1:] {
 		maps = append(maps, parseMap(region))
 	}
-	result := []Step{}
-	for _, seed := range seeds {
-		// solve(seed, maps, Location)
-		result = append(result, solve(seed, maps, Location))
+
+	return solve(seeds, maps)
+}
+
+func part2(input string) int {
+	regions := utils.GetRegions(input)
+	seeds := parseSeeds2(regions[0][0])
+	maps := []Routemap{}
+	for _, region := range regions[1:] {
+		maps = append(maps, parseMap(region))
 	}
+
+	return solve(seeds, maps)
+}
+
+func solve(seeds []Step, maps []Routemap) int {
+	result := []Step{}
+	results := make(chan Step, len(seeds))
+	for _, seed := range seeds {
+		go func(seed Step) {
+			results <- solveSeed(seed, maps, Location)
+		}(seed)
+	}
+
+	for i := 0; i < len(seeds); i++ {
+		result = append(result, <-results)
+	}
+	close(results)
+
 	min := result[0].Value
-	// get the lowest value in result
+
 	for _, step := range result {
-		// get the lowest value in result
+
 		if step.Value < min {
 			min = step.Value
 		}
@@ -44,7 +68,7 @@ func part1(input string) int {
 *
 Run through the seeds, and for each seed, run through the maps until the step's type matches the stage's type.
 */
-func solve(seed Step, maps []Routemap, stage Stage) Step {
+func solveSeed(seed Step, maps []Routemap, stage Stage) Step {
 	for _, routemap := range maps {
 		seed = routemap.Solve(seed)
 	}
@@ -56,6 +80,27 @@ func parseSeeds(input string) []Step {
 	for _, part := range strings.Split(input, " ")[1:] {
 		value, _ := strconv.Atoi(part)
 		result = append(result, Step{Value: value, Type: Seed})
+	}
+	return result
+}
+
+/*
+the first number is the seed, the second is the length of the seed.
+example: 3 2 means the seed is 3, and the length is 2.
+this means that the seed is 3, 4, 5.
+*/
+func parseSeeds2(input string) []Step {
+	var result []Step
+	var values []int
+	for _, part := range strings.Split(input, " ")[1:] {
+		value, _ := strconv.Atoi(part)
+		values = append(values, value)
+	}
+	for i := 0; i < len(values); i += 2 {
+		result = append(result, Step{Value: values[i], Type: Seed})
+		for j := 1; j < values[i+1]; j++ {
+			result = append(result, Step{Value: values[i] + j, Type: Seed})
+		}
 	}
 	return result
 }
